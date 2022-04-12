206ecdfd31bb12dabf7c01230a25ef6b5e77bd8e
Lucas Rodriguez <lucas.rodriguez@etu.u-paris.fr>
2022-04-12 12:02:48 +0200

    fix start game in main menu, start a pause function

--- src/view/Gui.java
+++ src/view/Gui.java
@@ -6,8 +6,10 @@ import model.GamePVP;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Gui extends JFrame{
+public class Gui extends JFrame implements KeyListener{
     private GuiMenu guiMenu;
     private GuiBar guiBar;
     private GuiBoard guiBoard;
@@ -15,6 +17,8 @@ public class Gui extends JFrame{
     public static int height; 
     private Board board;
     private Game game;
+    public static Thread gameThread;
+    public static boolean isPaused=false;
 
     public Gui(){
         width = 600;
@@ -23,6 +27,8 @@ public class Gui extends JFrame{
 
         setSize(600,553);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        addKeyListener(this);
 
         this.setLayout(new BorderLayout());        
         this.add(guiMenu,BorderLayout.CENTER);
@@ -50,12 +56,44 @@ public class Gui extends JFrame{
         guiBar.setPreferredSize(new Dimension(this.getHeight()/15,this.getWidth()/15));        
         this.add(guiBar,BorderLayout.NORTH);
 		this.add(guiBoard,BorderLayout.CENTER);
-        revalidate();
-        
-        game.gameLoop();
-		
+        requestFocusInWindow();
+
+        gameThread=new Thread(new Runnable(){
+            @Override
+            public void run(){
+                game.gameLoop();
+            }
+        });
+
+        gameThread.start();
 	}
 	public static void main(String[] args) {
 		Gui gui = new Gui();
 	}
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent){
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent){
+        int k = keyEvent.getKeyCode();
+        if(k==KeyEvent.VK_ESCAPE){
+            if(Gui.isPaused){
+                Gui.isPaused=false;
+                Gui.gameThread.resume();
+                System.out.println("resume");
+            } else{
+                Gui.isPaused=true;
+                Gui.gameThread.stop();
+                System.out.println("stop");
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent){
+
+    }
 }
